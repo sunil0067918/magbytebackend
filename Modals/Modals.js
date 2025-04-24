e0e381c2971d258d6422f9c953544981302a2c24
sunil7458 <sunilkumarmehra73@gmail.com>
2025-04-24 16:07:46 +0530

    sunil

--- Modals/Modals.js
+++ Modals/Modals.js
@@ -1,6 +1,122 @@
 const mongoose = require("mongoose");
+const ConsultationFormSchema = new mongoose.Schema({
+  inquryfirstname: {
+    type: String,
+    require: true,
+  },
 
-// Employee schema definition
+  inqueryemail: {
+    type: String,
+    require: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please fill a valid email address",
+    ],
+  },
+
+  inqueynumber: {
+    type: Number,
+    require: true,
+    match: [
+      /^\+?\d{1,4}?[.\-\s]?\(?\d{1,3}?\)?[.\-\s]?\d{1,4}[.\-\s]?\d{1,4}$/,
+      "Please fill a valid phone number",
+    ],
+  },
+  inqurymessage: {
+    type: String,
+    require: true,
+  },
+  inquerylastname: {
+    type: String,
+    require: true,
+  }
+});
+
+
+
+const inquerFormSchema = new mongoose.Schema({
+  inquryfirstname: {
+    type: String,
+    require: true,
+  },
+
+  inqueryemail: {
+    type: String,
+    require: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please fill a valid email address",
+    ],
+  },
+
+  inqueynumber: {
+    type: Number,
+    require: true,
+    match: [
+      /^\+?\d{1,4}?[.\-\s]?\(?\d{1,3}?\)?[.\-\s]?\d{1,4}[.\-\s]?\d{1,4}$/,
+      "Please fill a valid phone number",
+    ],
+  },
+  inqurymessage: {
+    type: String,
+    require: true,
+  },
+  inquerylastname: {
+    type: String,
+    require: true,
+  }
+});
+
+const contactFormSchema = new mongoose.Schema({
+  Firstname: {
+    type: String,
+    require: true,
+  },
+
+  Contactemail: {
+    type: String,
+    require: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please fill a valid email address",
+    ],
+  },
+
+  Contactnumber: {
+    type: Number,
+    require: true,
+    match: [
+      /^\+?\d{1,4}?[.\-\s]?\(?\d{1,3}?\)?[.\-\s]?\d{1,4}[.\-\s]?\d{1,4}$/,
+      "Please fill a valid phone number",
+    ],
+  },
+  textareamessage: {
+    type: String,
+    require: true,
+  },
+});
+
+const hireform = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: Number,
+    required: true,
+  },
+
+  jobposition: {
+    type: String,
+    required: true,
+  },
+});
+
 const employeeSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -25,15 +141,12 @@ const employeeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
-
-  // Add other fields as necessary
 });
 
-
-
-
-
-
-// Export the Employee model
-module.exports = mongoose.model("Employee", employeeSchema);
+module.exports = {
+  Hiref: mongoose.model("Hiref", hireform),
+  Employee: mongoose.model("Employee", employeeSchema),
+  Contact: mongoose.model("Contact", contactFormSchema),
+  Inqueryform : mongoose.model("Inquery" , inquerFormSchema),
+  Consultionform  :  mongoose.model("Consultionform" ,  ConsultationFormSchema ) 
+};
--- index.js
+++ index.js
@@ -1,65 +1,27 @@
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const Employee = require("./Modals/Modals");
-// const http =  require("http")
-// const nodemailer  =  require("nodemailer")
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// // MongoDB connection string
-// mongoose
-//   .connect(
-//     "mongodb+srv://sunil1234:Sunilmehra1234@cluster0.gg8va.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-//   )
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log("MongoDB connection error: ", err));
-
-// // POST route to create a new Employee
-// // POST route to create a new Employee
-// app.post("/register", async (req, res) => {
-//   try {
-//     // Validate the request body (example validation)
-//     const { company, Email, firstName , requriement , number } = req.body;
-
-//     if (!firstName || !Email || !number  || !company || !requriement) {
-//       return res.status(400).json({ error: "All fields (name, email, position) are required." });
-//     }
-
-//     // Create a new employee in the database
-//     const employee = await Employee.create(req.body);
-
-//     // Return the created employee object in the response
-//     return res.status(201).json({
-//       message: "Employee registered successfully",
-//       employee,
-//     });
-//   } catch (err) {
-//     // Catch any errors and send a response with the error message
-//     console.error("Error in registering employee:", err);
-//     return res.status(500).json({ error: "Failed to register employee. Please try again." + err });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log("server is running");
-// });
-
-
-
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Employee = require("./Modals/Modals");
-const nodemailer = require("nodemailer");
+const {
+  Hiref,
+  Employee,
+  Contact,
+  Inqueryform,
+  Consultionform,
+} = require("./Modals/Modals");
 
+const nodemailer = require("nodemailer");n
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "http://145.223.21.29/"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(
@@ -67,41 +29,29 @@ mongoose
   )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error: ", err));
-
-// Create a nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail", // You can use your email service, e.g., 'gmail', 'hotmail', etc.
+  service: "gmail",
   auth: {
-    user: "s0067918@gmail.com", // Your email address
-    pass: "oczg bcan xclp rjbk", // Your email password or app password
-
+    user: "s0067918@gmail.com",
+    pass: "wvia ufem tzno ecxk",
   },
 });
 
-// POST route to create a new Employee
 app.post("/register", async (req, res) => {
   try {
-    // Validate the request body
     const { company, Email, firstName, requriement, number } = req.body;
-
     if (!firstName || !Email || !number || !company || !requriement) {
       return res
         .status(400)
         .json({ error: "All fields (name, email, position) are required." });
     }
-
-    // Create a new employee in the database
     const employee = await Employee.create(req.body);
-
-    // Send a confirmation email to the employee
     const mailOptions = {
-      from: "s0067918@gmail.com", // Sender address
-      to: Email, // Recipient address (from the request)
+      from: "s0067918@gmail.com",
+      to: Email,
       subject: "Employee Registration Successful",
       text: `Dear ${firstName},\n\nYou have been successfully registered as an employee at ${company}. We look forward to working with you!`,
     };
-
-    // Send the email using nodemailer
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.error("Error sending email:", error);
@@ -109,20 +59,187 @@ app.post("/register", async (req, res) => {
         console.log("Email sent:", info.response);
       }
     });
-
-    // Return the created employee object in the response
     return res.status(201).json({
-      message: "Employee registered successfully",
+      message: " customer registered successfully",
       employee,
     });
   } catch (err) {
-    // Catch any errors and send a response with the error message
-    console.error("Error in registering employee:", err);
+    console.error("Error in registering  Customer:", err);
     return res
       .status(500)
-      .json({ error: "Failed to register employee. Please try again." + err });
+      .json({ error: "Failed to register Customer. Please try again." + err });
   }
 });
+
+// second form  //
+app.post("/hireresister", async (req, res) => {
+  try {
+    const { fullName, email, phone, jobposition } = req.body;
+    if (!fullName || !email || !phone || !jobposition) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+    const newJobApplication = new Hiref({
+      fullName,
+      email,
+      phone,
+      jobposition,
+    });
+    const href = await newJobApplication.save();
+    const mailOptionshire = {
+      from: "s0067918@gmail.com",
+      to: email,
+      subject: "Employee Registration Successful",
+      text: `Dear ${fullName},\n\n  Your resume has been shortlisted. The HR team will contact you soon`,
+    };
+
+    transporter.sendMail(mailOptionshire, (error, info) => {
+      if (error) {
+        console.log("email sending message", error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+
+    res.status(201).json({
+      message: "The HR team will reach out to you shortly",
+      href,
+      status: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "Error processing the job application", status: false });
+  }
+});
+
+// third Form api / /
+
+app.post("/contactregister", async (req, res) => {
+  try {
+    const { Firstname, Contactemail, Contactnumber, textareamessage } =
+      req.body;
+    if (!Firstname || !Contactemail || !Contactnumber || !textareamessage) {
+      return res.status(400).json({ error: "all fieldare required " });
+    }
+    const newJobApplicationContact = new Contact({
+      Firstname,
+      Contactemail,
+      Contactnumber,
+      textareamessage,
+    });
+    const Contactf = await newJobApplicationContact.save();
+
+    const mailOptionsContact = {
+      from: "s0067918@gmail.com",
+      to: Contactemail,
+      subject: "Employee Registration Successful",
+      text: `Dear sir ,  ${Firstname},\n\n  our team contact you  `,
+    };
+
+    transporter.sendMail(mailOptionsContact, (error, info) => {
+      if (error) {
+        console.log("email sending message ", error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+    res.status(201).json({
+      message: " our team Contact you",
+      Contactf,
+      status: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: " Error processing the job application", status: false });
+  }
+});
+
+// forth Form  ///
+app.post("/inquerform", async (req, res) => {
+  try {
+    const {
+      inquryfirstname,
+      inqueryemail,
+      inquerylastname,
+      inqueynumber,
+      inqurymessage,
+    } = req.body;
+
+    if (
+      !inquryfirstname ||
+      !inqueryemail ||
+      !inquerylastname ||
+      !inqueynumber ||
+      !inqurymessage
+    ) {
+      return res.status(400).json({ error: "all field are required" });
+    }
+
+    const newJobApplicationInquery = new Inqueryform({
+      inquryfirstname,
+      inqueryemail,
+      inquerylastname,
+      inqueynumber,
+      inqurymessage,
+    });
+    const Inqueryf = await newJobApplicationInquery.save();
+
+    res.status(201).json({
+      message: " our team contact you",
+      Inqueryf,
+      status: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: " Error processing the job application", status: false });
+  }
+});
+
+app.post("/Consultresister", async (req, res) => {
+  try {
+    const {
+      Consultationfirstname,
+      Consultationemail,
+      Consultationnumber,
+      Consultationmessage,
+    } = req.body;
+
+    if (
+      !Consultationfirstname ||
+      !Consultationemail ||
+      !Consultationnumber ||
+      !Consultationmessage
+    ) {
+      return res.status(400).json({ error: "all field are required" });
+    }
+
+    const newJobApplicationConsult = new Consultionform({
+      Consultationfirstname,
+      Consultationemail,
+      Consultationnumber,
+      Consultationmessage,
+    });
+    const Consult = await newJobApplicationConsult.save();
+
+    res.status(201).json({
+      message: " our team contact you",
+      Consult,
+      status: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: " Error processing the job application", status: false });
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log("server is running");
